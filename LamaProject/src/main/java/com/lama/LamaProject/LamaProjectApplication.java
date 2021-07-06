d21937fad52915d211501d4475fdd91407a92176
AleksandarBudimirovic <aleksandar.budimirovic97@gmail.com>
2021-07-06 14:15:39 +0200

    init

---
+++ LamaProject/src/main/java/com/lama/LamaProject/LamaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lama.LamaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LamaProjectApplication.class, args);
+	}
+
+}
