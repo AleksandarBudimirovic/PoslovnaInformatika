6f010ec9350afafc0d840f912b3328ebb96ec952
aleksandra49 <anja.9811@gmail.com>
2021-07-07 01:56:18 +0200

    Napravljena klasa dto za robu

--- LamaProject/src/main/java/com/lama/LamaProject/dto/RobaDTO.java
+++ LamaProject/src/main/java/com/lama/LamaProject/dto/RobaDTO.java
@@ -1,5 +1,12 @@
 package com.lama.LamaProject.dto;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.lama.LamaProject.main.GrupaRobe;
+import com.lama.LamaProject.main.StavkaIzlaznaFaktura;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +15,19 @@ import lombok.Setter;
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class RobaDTO {
 	
-	
+	private long id;
+
+	private String nazivRobe;
+
+	private String jedinicaMere;
+	@JsonIgnore
+	private GrupaRobe grupaRobe;
+	@JsonIgnore
+	private Set<StavkeCenovnika> stavkeCenovnika = new HashSet<>();
+	@JsonIgnore
+	private Set<StavkaIzlaznaFaktura> stavkeFakture = new HashSet<>();
+
+	private boolean obrisano;
 	
 
 }
